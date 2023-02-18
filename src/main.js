b48593d316c53a25acf214b08c2bd22a77a208eb
jeissoR115 <jeissonfeliper@gmail.com>
2023-02-17 19:20:36 -0500

    Prototipos

--- src/main.js
+++ src/main.js
@@ -10,3 +10,21 @@ const jeisson = {
         this.approvedCourses.push(newCourse)
     }
 };
+
+// prototipo("class")
+function Student(name, userName,  age, approvedCourses){
+    //argumentos
+    this.name= name
+    this.userName= userName
+    this.age=age
+    this.approvedCourses= approvedCourses
+}
+    //metodo
+    Student.prototype.approvedCourses = function(newCourse) {
+        this.approvedCourses.push(newCourse)
+    }
+
+// instancia de prototipo ("objet")
+const daniel = new Student('Daniel','Bananen',13,['Desarrollo de videojuegos','introduccion a gameMaker'])
+
+
