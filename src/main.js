c76eab617c3a0448811fcfcbf13f23dd16cd020b
jeissoR115 <jeissonfeliper@gmail.com>
2023-02-17 19:03:44 -0500

    Objetos literales

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// obj literal
+const jeisson = {
+    //argumentos
+    name: 'jeisson',
+    userName: 'JeissonR115',
+    age: 18,
+    approvedCourses: [],
+    //metodo
+    passCourse(newCourse) {
+        this.approvedCourses.push(newCourse)
+    }
+};
