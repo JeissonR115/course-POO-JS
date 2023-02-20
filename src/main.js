0b3be378d8b0cd95a2292c6bbd9fb48c68644ed1
jeissoR115 <jeissonfeliper@gmail.com>
2023-02-19 20:40:27 -0500

    obj literal como parametro en el constructor

--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ function Student(name, userName,  age, approvedCourses){
         this.approvedCourses.push(newCourse)
     }
 
-// instancia de prototipo ("objet")
+// instancia de prototipo ('objet')
 const daniel = new Student('Daniel','Bananen',13,['Desarrollo de videojuegos','introduccion a gameMaker'])
 
 
@@ -40,4 +40,27 @@ class Student2{
         this.approvedCourses.push(newCourse)
     }
 }
-const migel = new Student2("migel","m!g31",29,["Analisis de Negosios para la ciencia de datos","Redes informaticas de internet"])
+const migel = new Student2('migel','m!g31',29,['Analisis de Negosios para la ciencia de datos','Redes informaticas de internet'])
+
+//obj literal como parametro en el constructor
+class Student3{
+    constructor({name, userName,  age, approvedCourses,email,twitter}){
+        this.name= name
+        this.userName= userName
+        this.age=age
+        this.email = email
+        this.approvedCourses= approvedCourses
+        this.twitter= twitter
+
+    }
+    approvedCourses(newCourse){
+        this.approvedCourses.push(newCourse)
+    }
+}
+const angel = new Student3({
+    name: 'angel',
+    userName:'angelol',  
+    age: 7,
+    email:"angelortiz1129@gmail.com",
+    approvedCourses:['Proyectos Creativos para Niñas y Niños','Básico de JavaScript']
+})
