2da49768ed33f8ea1eed2a39ceab6fdddceabe89
jeissoR115 <jeissonfeliper@gmail.com>
2023-02-19 20:26:32 -0500

    "Clases"

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ const jeisson = {
     }
 };
 
-// prototipo("class")
+// prototipo
 function Student(name, userName,  age, approvedCourses){
     //argumentos
     this.name= name
@@ -28,3 +28,16 @@ function Student(name, userName,  age, approvedCourses){
 const daniel = new Student('Daniel','Bananen',13,['Desarrollo de videojuegos','introduccion a gameMaker'])
 
 
+// clases (prototipo con sintaxix distinta)
+class Student2{
+    constructor(name, userName,  age, approvedCourses){
+        this.name= name
+        this.userName= userName
+        this.age=age
+        this.approvedCourses= approvedCourses
+    }
+    approvedCourses(newCourse){
+        this.approvedCourses.push(newCourse)
+    }
+}
+const migel = new Student2("migel","m!g31",29,["Analisis de Negosios para la ciencia de datos","Redes informaticas de internet"])
